7e8aa625aa1b4022c4cdd18cedcd5a26137b37f6
Your Name <you@example.com>
2021-12-21 17:58:50 +0100

    budowa bazy danych i tabela na str Konr.

---
+++ SQL/Slowniki/CT_slownikPodmiot.sql
@@ -0,0 +1,11 @@
+
+
+CREATE TABLE [dbo].[slownikPodmiot](
+    [IdPodmiot] [int] IDENTITY(1,1) NOT NULL,
+    [Opis] [nvarchar](255) NULL
+    PRIMARY KEY CLUSTERED
+(
+[IdPodmiot] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+    ) ON [PRIMARY]
+    GO
